0023db74b5b4d623da4e14136fe9d49df8229d96
CHANG <jc.jonathan.chang94@gmail.com>
2025-10-16 11:07:42 +0200

    avance13

--- src/public/script.js
+++ src/public/script.js
@@ -1,7 +1,7 @@
 async function chargerCitation() { 
   const response = await fetch('https://zenquotes.io/api/random');
   const data = await response.json();
-  document.getElementById('citation').innerText = `${data.content} — ${data.author}`;
+  document.getElementById('citation').innerText = `${data.c} — ${data.a}`;
 }
 
 document.getElementById('nouvelle').addEventListener('click', chargerCitation);
