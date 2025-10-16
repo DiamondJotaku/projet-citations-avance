806b2c4589d53e4bcb0a3cbc1d88f6e222ff3805
DiamondJotaku <DiamondJotaku@users.noreply.github.com>
2025-10-16 08:33:26 +0000

    deploy: 69d0768f2fac6adbeb5bcc846a5c57b0e4d81c6a

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 async function chargerCitation() { 
   const response = await fetch('https://zenquotes.io/api/random');
   const data = await response.json();
-  document.getElementById('citation').innerText = `${ data[0].q } ${ data[0].a }`;
+  document.getElementById('citation').innerText = `${data[0].q} — ${data[0].a}`;
 }
 
-document.getElementById('nouvelle').addEventListener('click',chargerCitation);
-window.onload = "chargerCitation";
+document.getElementById('nouvelle').addEventListener('click', chargerCitation);
+window.onload = chargerCitation;
